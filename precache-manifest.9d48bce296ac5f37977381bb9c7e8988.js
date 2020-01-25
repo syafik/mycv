1ba819ad4376e10a46fa3cf739624186b09d06e6
syafik <syafikli_musyafako@yahoo.com>
2020-01-25 19:31:37 +0700

    Updates

---
+++ precache-manifest.9d48bce296ac5f37977381bb9c7e8988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b1166fad4af45a53840cab33b8297b",
+    "url": "/mycv/index.html"
+  },
+  {
+    "revision": "7e3953a48fcaa9b51141",
+    "url": "/mycv/static/css/main.16e18a48.chunk.css"
+  },
+  {
+    "revision": "a9af0e0fe8e414fe66aa",
+    "url": "/mycv/static/js/2.dd390ce7.chunk.js"
+  },
+  {
+    "revision": "74ac62a345da381fec9d77a57940d93b",
+    "url": "/mycv/static/js/2.dd390ce7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7e3953a48fcaa9b51141",
+    "url": "/mycv/static/js/main.a26625a8.chunk.js"
+  },
+  {
+    "revision": "4bbac17edbe0635082db",
+    "url": "/mycv/static/js/runtime-main.db75fcb6.js"
+  }
+]);
