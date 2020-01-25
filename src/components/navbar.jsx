d2532ff24bbf0d1d325c43fc3996081dcdebd795
syafik <syafikli_musyafako@yahoo.com>
2020-01-25 19:43:47 +0700

    adding jquery

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ export default class Navbar extends Component {
 	  return (
          <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
         <a className="navbar-brand js-scroll-trigger" href="#page-top">
-          <span className="d-block d-lg-none">Clarence Taylor</span>
+          <span className="d-block d-lg-none">Syafikli Musyafako</span>
           <span className="d-none d-lg-block">
             <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={process.env.PUBLIC_URL+'/img/smpoto2.jpg'} alt="" />
           </span>
